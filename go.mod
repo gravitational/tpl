e41a4d06e5d506c94233c8b26f09a1afb00c21a6
dmitri <deemok@gmail.com>
2020-02-03 14:05:37 +0100

    Allow output to a file - this will allow to handle errors in template w/o leaving an empty file behind

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gravitational/trace v1.1.9
 	github.com/jonboulle/clockwork v0.1.0 // indirect
-	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/crypto v0.0.0-20200128174031-69ecbb4d6d5d // indirect
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
 	"io"
-	"log"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/BurntSushi/toml"
 	"github.com/gravitational/trace"
+	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		log.Fatalln("Use: tpl <values-file> <template-file> [<template-file>...]")
-	}
+var valuesFile = flag.String("values", "", "File with values")
+var outputFile = flag.String("output", "", "Output file path. Defaults to stdout if unspecified")
+var templateFiles stringList
 
-	valuesFile := os.Args[1]
-	if err := run(valuesFile, os.Args[2:]...); err != nil {
+func init() {
+	flag.Var(&templateFiles, "template", "Template file. Can be specified multiple times")
+}
+
+func main() {
+	flag.Parse()
+	if *valuesFile == "" {
+		log.Error("values file is required")
+		flag.Usage()
+	}
+	if err := run(*valuesFile, *outputFile, templateFiles...); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func run(valuesFile string, templateFiles ...string) error {
+func run(valuesFile, outputFile string, templateFiles ...string) error {
 	valsf, err := os.Open(valuesFile)
 	if err != nil {
 		return trace.ConvertSystemError(err)
@@ -35,7 +49,15 @@ func run(valuesFile string, templateFiles ...string) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	return tpl.Execute(os.Stdout, v)
+	if outputFile == "" {
+		return tpl.Execute(os.Stdout, v)
+	}
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, v)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return copyReaderWithPerms(outputFile, &buf, sharedReadWriteMask)
 }
 
 func decodeValues(valuesFile io.Reader) (v interface{}, err error) {
@@ -45,3 +67,55 @@ func decodeValues(valuesFile io.Reader) (v interface{}, err error) {
 	}
 	return v, nil
 }
+
+// String formats this list for output
+func (r stringList) String() string {
+	return fmt.Sprint(([]string)(r))
+}
+
+func (r *stringList) Set(v string) error {
+	if strings.TrimSpace(v) == "" {
+		return trace.BadParameter("value cannot be empty")
+	}
+	*r = append(*r, v)
+	return nil
+}
+
+// copyReaderWithPerms copies the contents from src to dst atomically.
+// If dst does not exist, CopyReaderWithPerms creates it with permissions perm.
+// If the copy fails, CopyReaderWithPerms aborts and dst is preserved.
+// Adopted with modifications from https://go-review.googlesource.com/#/c/1591/9/src/io/ioutil/ioutil.go
+func copyReaderWithPerms(dst string, src io.Reader, perm os.FileMode) error {
+	tmp, err := ioutil.TempFile(filepath.Dir(dst), "")
+	if err != nil {
+		return trace.ConvertSystemError(err)
+	}
+	defer func() {
+		if err == nil {
+			return
+		}
+		if err := os.Remove(tmp.Name()); err != nil {
+			log.Errorf("Failed to remove %v: %v.", tmp.Name(), err)
+		}
+	}()
+	_, err = io.Copy(tmp, src)
+	if err != nil {
+		tmp.Close()
+		return trace.ConvertSystemError(err)
+	}
+	if err = tmp.Close(); err != nil {
+		return trace.ConvertSystemError(err)
+	}
+	if err = os.Chmod(tmp.Name(), perm); err != nil {
+		return trace.ConvertSystemError(err)
+	}
+	err = os.Rename(tmp.Name(), dst)
+	if err != nil {
+		return trace.ConvertSystemError(err)
+	}
+	return nil
+}
+
+type stringList []string
+
+const sharedReadWriteMask = 0666
