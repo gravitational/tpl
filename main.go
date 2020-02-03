290e951fed9b415627022c6c79c8c5eac5eaf437
dmitri <deemok@gmail.com>
2020-02-03 13:22:37 +0100

    Render from multiple template files

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"text/template"
@@ -12,18 +11,17 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatalln("Use: tpl <template-file> <values-file>")
+	if len(os.Args) < 3 {
+		log.Fatalln("Use: tpl <values-file> <template-file> [<template-file>...]")
 	}
 
-	templateFile := os.Args[1]
-	valuesFile := os.Args[2]
-	if err := run(templateFile, valuesFile); err != nil {
+	valuesFile := os.Args[1]
+	if err := run(valuesFile, os.Args[2:]...); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func run(templateFile, valuesFile string) error {
+func run(valuesFile string, templateFiles ...string) error {
 	valsf, err := os.Open(valuesFile)
 	if err != nil {
 		return trace.ConvertSystemError(err)
@@ -33,16 +31,7 @@ func run(templateFile, valuesFile string) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	tmplf, err := os.Open(templateFile)
-	if err != nil {
-		return trace.ConvertSystemError(err)
-	}
-	defer tmplf.Close()
-	tplContents, err := ioutil.ReadAll(tmplf)
-	if err != nil {
-		return trace.ConvertSystemError(err)
-	}
-	tpl, err := template.New("tpl").Parse(string(tplContents))
+	tpl, err := template.ParseFiles(templateFiles...)
 	if err != nil {
 		return trace.Wrap(err)
 	}
